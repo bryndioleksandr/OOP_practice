704e6f00ef2e7e781af3e5805da40ae84b1dfbea
bryndioleksandr <s.v.bryndo@gmail.com>
2023-10-23 19:25:30 +0300

    Header and .cpp files are created. Class Patient added

---
+++ Практична робота №1/Patient.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class Patient {
+	int id;
+	string surname;
+	string firstName;
+	string lastName;
+	string address;
+	string phoneNumber;
+	string medicalNumber;
+	string diagnosis;
+};
